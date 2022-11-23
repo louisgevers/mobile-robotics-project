a5d58c6b7a8cc53bd3704be67b8b7b9b22619fb5
Louis Gevers <louis.gevers@gmail.com>
2022-11-23 19:33:06 +0100

    WIP: calibrate

--- src/vision.py
+++ src/vision.py
@@ -1,4 +1,5 @@
 from src import model
+import cv2
 
 
 def analyze_scene() -> model.World:
@@ -11,3 +12,37 @@ def analyze_scene() -> model.World:
         goal=model.Point(0, 0),
         obstacles=[],
     )
+
+
+def get_webcam_capture(builtin: bool) -> cv2.VideoCapture:
+    index = 0 if builtin else 2
+    return cv2.VideoCapture(index)
+
+
+def main():
+
+    source = get_webcam_capture(builtin=False)
+
+    while True:
+
+        # Capture the video frame
+        # by frame
+        ret, frame = source.read()
+
+        # Display the resulting frame
+        cv2.imshow("frame", frame)
+
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
