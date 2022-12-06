df575fc46e0e34431b4aa2c762c946b77a194259
ShawnTan020226 <118297814+ShawnTan020226@users.noreply.github.com>
2022-12-06 17:01:49 +0100

    Update filtering.py

--- src/filtering.py
+++ src/filtering.py
@@ -6,9 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def update_robot(
-    robot: model.Robot, command: model.MotorSpeed, sensors: model.SensorReading
+    robot: model.Robot, command: model.MotorSpeed, sensors: model.SensorReading, states
 ):
-    pass
+    
+    
+    x,P=states.Kalmanfilter([command.left,command.right],np.concatenate(robot.position,robot.angle,sensors.motor.left,sensors.motor.right),True)
+    robot.position.x=x[0]
+    robot.position.y=x[1]
+    robot.angle=x[2]
+    
 class filter:
     def __init__(self,x0,P0,Q,R,T):
         
@@ -24,21 +30,7 @@ class filter:
         self.u_prev=np.array([0,0])
         self.speedconv=0.434782608
         
-    def parameter(self,coord_ini,coord_end, orientation_ini, orientation_end):
-        a=1
-        #parameters required:
-        #speed multiplier
-        #angular speed multiplier
-        #
-        #Measurement y
-        #H
-        
-        
-        
-        
-        self.Q=Q
-        self.R=R
-        
+
         
 
     def Kalmanfilter(self,u,speed,camerapos,camera=False):
@@ -101,3 +93,20 @@ class filter:
         self.P.append(P_est.tolist())
         return x_est,P_est
 
+L=1
+picture=True
+thetadotvar=0.6
+speedvar=6.15
+posvar=0.1
+thetavar=0.01
+measvar=np.diag([0.02,0.02,0.02/L,6.15,6.15/L])
+statevar=np.diag([posvar,posvar,thetavar,speedvar,speedvar/L])
+iter=30
+if picture:
+    i=5
+else:
+    i=2
+x=[]
+def initialise(initialposition,initialangle):
+    y=filter(np.concatenate(initialposition,initialangle,0,0),np.zeros((5,5)),measvar*np.eye(i),statevar,0.1)
+    return y
