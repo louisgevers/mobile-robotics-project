660fb21f169425d26a994a7a7336747caaaf8cc3
Louis Gevers <louis.gevers@gmail.com>
2022-12-07 14:19:35 +0100

    Latest labwork

--- src/filtering.py
+++ src/filtering.py
@@ -4,139 +4,141 @@ from src import model
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def update_robot(
+    robot: model.Robot, command: model.MotorSpeed, sensors: model.SensorReading, states
+):
+
+    x, P = states.Kalmanfilter(
+        np.array([command.left, command.right]),
+        np.array([sensors.motor.left, sensors.motor.right]),
+        np.array([robot.position.x, robot.position.y, robot.angle]),
+        True,
+    )
+    robot.position.x = x[0]
+    robot.position.y = x[1]
+    robot.angle = x[2]
+
+
 class filter:
-    def __init__(self,x0,P0,Q,R,T):
-        
-        self.x=[x0]
-        self.u=[]
-        self.P=[P0]
-        self.T=T
-        self.speed=[]
-        self.camerapos=[]
-        self.L=1
-        self.R=R
-        self.Q=Q
-        self.u_prev=np.array([0,0])
-        
-        
-    def parameter(self,coord_ini,coord_end, orientation_ini, orientation_end):
-        a=1
-        #parameters required:
-        #speed multiplier
-        #angular speed multiplier
-        #
-        #Measurement y
-        #H
-        
-        
-        
-        
-        self.Q=Q
-        self.R=R
-        
-        
+    def __init__(self, x0, P0, Q, R, T):
 
-    def Kalmanfilter(self,u,speed,camerapos,camera=False):
-        #x=[x,y,theta,xdot,thetadot]
-        #u=[vl,vr]
-        
-        udiff=u-self.u_prev
-        self.u_prev=u
+        self.x = [x0]
+        self.u = []
+        self.P = [P0]
+        self.T = T
+        self.speed = []
+        self.camerapos = []
+        self.L = 1
+        self.R = R
+        self.Q = Q
+        self.u_prev = np.array([0, 0])
+        self.speedconv = 0.434782608
+
+    def Kalmanfilter(self, u, speed, camerapos, camera=False):
+        # x=[x,y,theta,xdot,thetadot]
+        # u=[vl,vr]
+        print(u)
+        udiff = u - self.u_prev
+        self.u_prev = u
         # print(udiff)
-        x=np.array(self.x[-1]).T
-        Sigma=np.array(self.P[-1]).T
-        theta=x[2]
-        A=np.array([[1,0,0,np.cos(theta)*self.T,0],
-                    [0,1,0,np.sin(theta)*self.T,0],
-                    [0,0,1,0,self.T/self.L],
-                    [0,0,0,1,0],
-                    [0,0,0,0,1]])
+        x = np.array(self.x[-1]).T
+        Sigma = np.array(self.P[-1]).T
+        theta = x[2]
+        A = np.array(
+            [
+                [1, 0, 0, np.cos(theta) * self.T, 0],
+                [0, 1, 0, np.sin(theta) * self.T, 0],
+                [0, 0, 1, 0, self.T / self.L],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1],
+            ]
+        )
 
-        
-        B=np.zeros((5,2))
-        B[3:]=[[1,1],[-1,1]]
-        B=0.5*B
+        B = np.zeros((5, 2))
+        B[3:] = [[1, 1], [-1, 1]]
+        B = self.speedconv * 0.5 * B
         # print(B)
-        x_pred=A@x+B@udiff
+        x_pred = A @ x + B @ udiff
         # print(A@x)
         # print(x_pred)
-        G=np.array([[1,0,-0.5*self.T*np.sin(theta)*x[3],0.5*np.cos(theta)*self.T,0],
-           [0,1,0.5*self.T*np.cos(theta)*x[4],0.5*np.sin(theta)*self.T,0],
-           [0,0,1,0,self.T*0.5/self.L],
-           [0,0,0,1,0],
-           [0,0,0,0,1]])
+        G = np.array(
+            [
+                [
+                    1,
+                    0,
+                    -0.5 * self.T * np.sin(theta) * x[3],
+                    0.5 * np.cos(theta) * self.T,
+                    0,
+                ],
+                [
+                    0,
+                    1,
+                    0.5 * self.T * np.cos(theta) * x[4],
+                    0.5 * np.sin(theta) * self.T,
+                    0,
+                ],
+                [0, 0, 1, 0, self.T * 0.5 / self.L],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1],
+            ]
+        )
         # print(G)
         # print(Sigma)
-        P_new=G@Sigma@G.T +self.R
-        P_est=P_new
+        P_new = G @ Sigma @ G.T + self.R
+        P_est = P_new
         # print(P_new)
         if camera:
             # print(camerapos,speed)
-            measurement=np.append(camerapos,speed)
-            M=np.eye(5)
-            M[3:,3:]=0.5*np.array([[1,1],[-1,1]])
-            C=np.eye(5)
+            measurement = np.append(camerapos, speed)
+            M = np.eye(5)
+            M[3:, 3:] = self.speedconv * 0.5 * np.array([[1, 1], [-1, 1]])
+            C = np.eye(5)
         else:
-            measurement=speed
-            M=[[0.5,0.5],[-0.5,0.5]]
-            C=np.concatenate((np.zeros((2,3)),np.eye(2)),axis=1)
-        y=M@measurement
-        K=P_new@C.T@(np.linalg.inv(C@P_new@C.T+self.Q))
+            measurement = speed
+            M = [[0.5, 0.5], [-0.5, 0.5]]
+            C = self.speedconv * np.concatenate((np.zeros((2, 3)), np.eye(2)), axis=1)
+        y = M @ measurement
+        K = P_new @ C.T @ (np.linalg.inv(C @ P_new @ C.T + self.Q))
         # C=np.concatenate((np.zeros((2,3)),np.eye(2)),axis=1)
 
-        x_est=x_pred+K@(y-C@x_pred)
-        
+        x_est = x_pred + K @ (y - C @ x_pred)
+
         # print(y)
         # print(C)
         # print(C@x)
-        a=K@C
-        P_est=(np.eye(np.shape(a)[0])-K@C)@P_new
+        a = K @ C
+        P_est = (np.eye(np.shape(a)[0]) - K @ C) @ P_new
 
         self.x.append(x_est.tolist())
         self.P.append(P_est.tolist())
-        return x_est,P_est
-R=np.eye(5)
-# R[3:,3:]=np.zeros((2,2))
-picture=True
-speedvar=2
-posvar=0.1
-thetavar=0.01
-measvar=0.1
-statevar=0.2
-iter=30
-if picture:
-    i=5
-    
-else:
-    i=2
-x=[]
-y=filter([0,0,0,0,0],np.zeros((5,5)),measvar*np.eye(i),statevar*R,0.1)
-speeds=np.array([12,12])
-coord=np.array([0,0,0])
-for i in range(iter):
-    xnext,Pnext=y.Kalmanfilter(speeds,np.random.normal(speeds,[speedvar,speedvar]),coord,picture)
-    coord=np.random.normal(xnext[:3],[posvar,posvar,thetavar])
-        
-    x.append(xnext.tolist())
-    # print(xnext)
-    # print(Pnext)
-speeds=np.array([11,-11])
-for i in range(iter):
-    xnext,Pnext=y.Kalmanfilter(speeds,np.random.normal(speeds,[speedvar,speedvar]),coord,picture)
-    coord=np.random.normal(xnext[:3],[posvar,posvar,thetavar])
-    
-    x.append(xnext.tolist())
-    # print(Pnext)
-speeds=np.array([18,9])
-for i in range(iter):
-    xnext,Pnext=y.Kalmanfilter(speeds,np.random.normal(speeds,[speedvar,speedvar]),coord,picture)
-    coord=np.random.normal(xnext[:3],[posvar,posvar,thetavar])
-    
-    x.append(xnext.tolist())
-    # print(Pnext[1,0])
-x=np.array(x)
-print(x)
-plt.plot(x[:,0],x[:,1])
-plt.show()
+        return x_est, P_est
 
-    
+
+R = np.eye(5)
+L = 1
+picture = True
+thetadotvar = 0.6
+speedvar = 6.15
+posvar = 0.1
+thetavar = 0.01
+measvar = np.diag([0.02, 0.02, 0.02 / L, 6.15, 6.15 / L])
+statevar = np.diag([posvar, posvar, thetavar, speedvar, speedvar / L])
+iter = 30
+if picture:
+    i = 5
+else:
+    i = 2
+x = []
+
+
+def initialise(initialposition, initialangle):
+    y = filter(
+        np.concatenate((initialposition, [initialangle], [0, 0])),
+        np.zeros((5, 5)),
+        measvar * np.eye(i),
+        statevar,
+        0.1,
+    )
+    return y
--- src/navlocal.py
+++ src/navlocal.py
@@ -1,67 +1,84 @@
 from src import model, thymio
 import time
 import math
+import numpy as np
 
 TRESHOLD_IR_SENSOR = 900
-TURN_SPEED = 50
-DELTA_ANGLE_SENSOR = 80/5 #angle between distance sensors (in degree)
-def set_motor_speed(th : thymio.Thymio, speed : model.MotorSpeed):
+TURN_SPEED = 150
+DELTA_ANGLE_SENSOR = 80 / 5  # angle between distance sensors (in degree)
+
+
+def set_motor_speed(th: thymio.Thymio, speed: model.MotorSpeed):
     command = speed
     th.process_command(command)
     return
 
-def avoid_obstacle(th : thymio.Thymio):
+
+def avoid_obstacle(th: thymio.Thymio, robot_position):
     sensor_data = th.read_sensor_data()
-    pos_data = th.read_robot_position()
+    pos_data = robot_position()
     last_speed_l = th.read_sensor_data()
     last_speed_r = th.read_sensor_data()
-    last_speed = (float(last_speed_l.motor.left)+float(last_speed_r.motor.right))/2 #problem here
+    last_speed = 100
+    # last_speed = (
+    #     float(last_speed_l.motor.left) + float(last_speed_r.motor.right)
+    # ) / 2  # problem here
     time.sleep(0.1)
     while sees_obstacle(sensor_data):
         sensor_data = th.read_sensor_data()
-        pos_data = th.read_robot_position()
+        pos_data = robot_position()
         last_angle_pos = pos_data.angle
-        sum_angle=0.
+        sum_angle = 0.0
         angle = get_angle(sensor_data)
         current_angle_pos = last_angle_pos
         while True:
-            current_angle_pos = th.read_robot_position().angle
-            delta = abs(current_angle_pos-last_angle_pos)
-            #if delta>3: uncomment if discontinuity problem
-            #    delta = delta - 2*3.14
-            last_angle_pos=current_angle_pos
-            sum_angle=sum_angle+delta
-            if angle>0:
-                set_motor_speed(th,calculate_speed(last_speed,TURN_SPEED))#try different values
+            current_angle_pos = robot_position().angle
+            delta = abs(current_angle_pos - last_angle_pos)
+            if delta > 3:
+                delta = delta - 2 * 3.14
+            last_angle_pos = current_angle_pos
+            sum_angle = sum_angle + delta
+            if angle > 0:
+                set_motor_speed(
+                    th, calculate_speed(last_speed, -TURN_SPEED)
+                )  # try different values
+
             else:
-                set_motor_speed(th,calculate_speed(last_speed,-TURN_SPEED))#try different values
-            if sum_angle>=angle:
-                set_motor_speed(th,calculate_speed(0,0))
+                set_motor_speed(
+                    th, calculate_speed(last_speed, TURN_SPEED)
+                )  # try different values
+            if sum_angle >= angle:
+                set_motor_speed(th, calculate_speed(last_speed, 0))
                 break
-            time.sleep(0.1)
     return
 
+
 def get_angle(sensor_data: model.SensorReading) -> float:
-    alpha = DELTA_ANGLE_SENSOR*math.pi/180
-    alpha_array =  [-2*alpha,1*alpha,0,1*alpha,2*alpha]
+    alpha = DELTA_ANGLE_SENSOR * math.pi / 180
+    alpha_array = [-2 * alpha, 1 * alpha, 0, 1 * alpha, 2 * alpha]
     tmp1 = 0
     tmp2 = 10e-7
     for i in range(5):
-        tmp1=tmp1+alpha_array[i]*get_array(sensor_data)[i]
-        tmp2=tmp2+get_array(sensor_data)[i]
-    return (tmp1/tmp2)
+        tmp1 = tmp1 + alpha_array[i] * get_array(sensor_data)[i]
+        tmp2 = tmp2 + get_array(sensor_data)[i]
+    return tmp1 / tmp2
+
 
 def calculate_speed(linear, angular) -> model.MotorSpeed:
-    rspeed = linear + angular/2
-    lspeed = linear - angular/2
+    rspeed = linear + angular / 2
+    lspeed = linear - angular / 2
     return model.MotorSpeed(left=rspeed, right=lspeed)
+
 
 def get_array(sensor_data: model.SensorReading):
     tmp = sensor_data.horizontal
-    return [tmp.left,tmp.center_left,tmp.center,tmp.center_right,tmp.right]
+    return np.array(
+        [tmp.left, tmp.center_left, tmp.center, tmp.center_right, tmp.right]
+    )
+
 
 def sees_obstacle(sensor_data: model.SensorReading) -> bool:
-    if any(get_array(sensor_data))>TRESHOLD_IR_SENSOR: #problem here
+    if any(get_array(sensor_data) > TRESHOLD_IR_SENSOR):  # problem here
         return True
     return False
 
@@ -71,6 +88,6 @@ if __name__ == "__main__":
     # Create the thymio connection
     th = thymio.Thymio()
     while True:
-        avoid_obstacle(th)  
+        avoid_obstacle(th)
     # Stop the robot
     th.stop()
