b31d47e375542842893b5bca329d63f71ed90f71
Louis Gevers <louis.gevers@gmail.com>
2022-12-10 21:30:55 +0100

    Clean up filtering code

--- src/filtering.py
+++ src/filtering.py
@@ -1,70 +1,76 @@
 from src import model
-
-
-import random
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def update_robot(
-    robot: model.Robot,
-    command: model.MotorSpeed,
-    sensors: model.SensorReading,
-    states,
-    camera,
-):
-    x, P = states.Kalmanfilter(
-        np.array([command.left, command.right]),
-        np.array([sensors.left, sensors.right]),
-        np.array([robot.position.x, robot.position.y, robot.angle]),
-        camera,
-    )
-    robot.position.x = x[0]
-    robot.position.y = x[1]
-    robot.angle = x[2]
+class Filter:
+    def __init__(
+        self,
+        initial_state: model.Robot,
+    ) -> None:
+        # Constants
+        self.L = 150
+        self.T = 0.1
+        SPEED_VAR = 6.15
+        SPEED_CONVERSION = 0.15
+        POS_VAR = 1
+        THETA_VAR = 0.01
 
-
-def initialise(initialposition, initialangle):
-    L = 150
-    thetadotvar = 0.6
-    speedvar = 6.15
-    posvar = 1
-    thetavar = 0.01
-    measvar = np.diag([1, 1, 1 / L, 0.434782608 * 6.15, 0.434782608 * 6.15 / L])
-    statevar = np.diag([posvar, posvar, thetavar, speedvar, speedvar / L])
-    y = filter(
-        np.concatenate((initialposition, [initialangle], [0, 0])),
-        np.zeros((5, 5)),
-        measvar,
-        statevar,
-        0.1,
-    )
-    return y
-
-
-class filter:
-    def __init__(self, x0, P0, Q, R, T):
-
-        self.x = [x0]
+        # Initialize variables for kalman filter
+        self.x = np.concatenate(
+            (initial_state.position.v, [initial_state.angle], [0, 0])
+        )
         self.u = []
-        self.P = [P0]
-        self.T = T
+        self.P = np.zeros((5, 5))
         self.speed = []
         self.camerapos = []
-        self.L = 150
-        self.R = R
-        self.Q = Q
+        self.R = np.diag(
+            [
+                POS_VAR,
+                POS_VAR,
+                THETA_VAR,
+                SPEED_VAR,
+                SPEED_VAR / self.L,
+            ]
+        )
+        self.Q = np.diag(
+            [
+                1,
+                1,
+                1 / self.L,
+                SPEED_CONVERSION * SPEED_VAR,
+                SPEED_CONVERSION * SPEED_VAR / self.L,
+            ]
+        )
         self.u_prev = np.array([0, 0])
         self.speedconv = 0.15
 
-    def Kalmanfilter(self, u, speed, camerapos=0, camera=False):
-        # x=[x,y,theta,xdot,thetadot]
-        # u=[vl,vr]
+    def update_robot(
+        self,
+        robot: model.Robot,
+        command: model.MotorSpeed,
+        sensors: model.SensorReading,
+        camera: bool = True,
+    ) -> model.Robot:
+        x = self.__kalman_filter(
+            np.array([command.left, command.right]),
+            np.array([sensors.motor.left, sensors.motor.right]),
+            np.array([robot.position.x, robot.position.y, robot.angle]),
+            camera,
+        )
+        return model.Robot(position=model.Point(x=x[0], y=x[1]), angle=x[2])
+
+    def __kalman_filter(
+        self,
+        u: np.ndarray,
+        speed: np.ndarray,
+        camerapos: np.ndarray,
+        camera: bool = False,
+    ) -> np.ndarray:
         udiff = u - self.u_prev
         self.u_prev = u
-        # print(udiff)
-        x = np.array(self.x[-1]).T
-        Sigma = np.array(self.P[-1]).T
+
+        x = self.x.T
+        Sigma = self.P.T
         theta = x[2]
         A = np.array(
             [
@@ -79,10 +85,9 @@ class filter:
         B = np.zeros((5, 2))
         B[3:] = [[0.5, 0.5], [-1 / self.L, 1 / self.L]]
         B = self.speedconv * B
-        # print(B)
+
         x_pred = A @ x + B @ udiff
-        # print(A@x)
-        # print(x_pred)
+
         G = np.array(
             [
                 [1, 0, -self.T * np.sin(theta) * x[3], np.cos(theta) * self.T, 0],
@@ -92,13 +97,11 @@ class filter:
                 [0, 0, 0, 0, 1],
             ]
         )
-        # print(G)
-        # print(Sigma)
+
         P_new = G @ Sigma @ G.T + self.R
         P_est = P_new
-        # print(P_new)
+
         if camera:
-            # print(camerapos,speed)
             measurement = np.append(camerapos, speed)
             M = np.eye(5)
             M[3:, 3:] = self.speedconv * np.array(
@@ -111,18 +114,15 @@ class filter:
             M = self.speedconv * np.array([[0.5, 0.5], [-1 / self.L, 1 / self.L]])
             C = np.concatenate((np.zeros((2, 3)), np.eye(2)), axis=1)
             Q = self.Q[3:, 3:]
+
         y = np.ravel(M @ measurement)
         K = P_new @ C.T @ (np.linalg.inv(C @ P_new @ C.T + Q))
-        # C=np.concatenate((np.zeros((2,3)),np.eye(2)),axis=1)
 
         x_est = x_pred + K @ (y - C @ x_pred)
 
-        # print(y)
-        # print(C)
-        # print(C@x)
         a = K @ C
         P_est = (np.eye(np.shape(a)[0]) - K @ C) @ P_new
 
-        self.x.append(x_est.tolist())
-        self.P.append(P_est.tolist())
-        return x_est, P_est
+        self.x = x_est
+        self.P = P_est
+        return x_est
