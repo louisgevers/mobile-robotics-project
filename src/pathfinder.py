1ff0cb037d9cf90a9375bc8bfe86a7dacbb07ab2
Louis Gevers <louis.gevers@gmail.com>
2022-12-11 09:31:40 +0100

    Extend obstacles past terrain
    Pyvizgraph does not support boundaries so as a hack we extend obstacles near
    the border way further such that these points are not suitable

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -1,13 +1,13 @@
-from typing import Sequence
+from typing import Sequence, Tuple
 import pyvisgraph as vg
 from src import model
 
 
-def find_path(world: model.World) -> Sequence[model.Point]:
+def find_path(world: model.World, resolution: Tuple[int, int]) -> Sequence[model.Point]:
     # 1. Convert model points to point objects for the visibility graph library
     polygons = []
     for obstacle in world.obstacles:
-        polygon = [convert_to_vg_point(point) for point in obstacle]
+        polygon = [convert_to_vg_point(point, resolution) for point in obstacle]
         polygons.append(polygon)
     # 2. Compute the visibility graph
     graph = vg.VisGraph()
@@ -21,8 +21,21 @@ def find_path(world: model.World) -> Sequence[model.Point]:
     return [convert_from_vg_point(point) for point in path]
 
 
-def convert_to_vg_point(point: model.Point) -> vg.Point:
-    return vg.Point(point.x, point.y)
+def convert_to_vg_point(
+    point: model.Point, resolution: Tuple[int, int] = None
+) -> vg.Point:
+    x = point.x if resolution is None else validate_border(point.x, resolution[0])
+    y = point.y if resolution is None else validate_border(point.y, resolution[1])
+    return vg.Point(x, y)
+
+
+def validate_border(p: int, r: int, err=10) -> int:
+    if p - err <= 0:
+        return -1000
+    elif p + err >= r:
+        return r + 1000
+    else:
+        return p
 
 
 def convert_from_vg_point(point: vg.Point) -> model.Point:
